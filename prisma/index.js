1a1df304b792d677f099e24dfe242017e03ffdef
Joy Harrington <joysyk23@gmail.com>
2024-10-21 17:16:39 -0500

    express setup and auth router

--- prisma/index.js
+++ prisma/index.js
@@ -1,5 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const prisma = new PrismaClient().$extends({
   model: {
     user: {
---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  next({ status: 404, message: "Endpoint not found." });
+});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.json(err.message ?? "Sorry, something broke :(");
+});
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
